7d7e899cb0122b01994cd88d0a91bb2922d0cd3f
manoharn725 <manoharnmanu725@gmail.com>
2023-11-18 09:18:49 +0530

    inside carsSlice initialState cars name as been changed to data

--- src/store/slices/carsSlice.js
+++ src/store/slices/carsSlice.js
@@ -1,33 +1,33 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const carsSlice = createSlice({
-    name: 'cars',
-    initialState: {
-        searchTerm: '',
-        cars: [],
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    data: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
     },
-    reducers: {
-        changeSearchTerm(state, action){
-            state.searchTerm = action.payload;
-        },
-        addCar(state, action){
-            //Assumption
-            //action.payload === { name:'' , cost: 140 }
-            state.cars.push({
-                name: action.payload.name,
-                cost: action.payload.cost,
-                id: nanoid(),
-            });
-        },
-        removeCar(state,action){
-            //Assumption
-            //action.payload === the id of the car we want to remove
-            const updated = state.cars.filter((car) => {
-                return car.id !== action.payload;
-            })
-            state.cars = updated;
-        },
-    }
+    addCar(state, action) {
+      //Assumption
+      //action.payload === { name:'' , cost: 140 }
+      state.data.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    removeCar(state, action) {
+      //Assumption
+      //action.payload === the id of the car we want to remove
+      const updated = state.data.filter((car) => {
+        return car.id !== action.payload;
+      });
+      state.data = updated;
+    },
+  },
 });
 
 export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
